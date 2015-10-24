c5c96c7085756f9c6d257e14e69d67ac170f21dc
Sam Kiswani <ssk09c@my.fsu.edu>
2015-10-24 13:15:01 -0400

    err um why different answers?

--- classf/likelihood.py
+++ classf/likelihood.py
@@ -1,14 +1,19 @@
 import numpy as np
-from scipy import stats
 import logging
 from typing import Iterable, Union
 from .core import Classifier
 
 logger = logging.getLogger(__name__)
 
-def make_discriminant(W, w, omega):
-    return lambda x: float(((x.T).dot(W)).dot(x) + (w.T).dot(x) + omega)
+def make_discriminant(mu, sigma, prior):
+    mat_mu = np.matrix(mu)
+    invSigma = np.linalg.inv(sigma)
 
+    Wi = np.matrix(-0.5 * invSigma)
+    wi = np.matrix(invSigma * mat_mu)
+    wi0 = -0.5*(mat_mu.T).dot(np.linalg.inv(sigma)).dot(mat_mu) - 0.5*np.log(np.linalg.det(sigma)) + np.log(prior)
+
+    return lambda x: np.float(-0.5*np.matrix(x).dot(Wi).dot(np.matrix(x).T) + (wi.T).dot(x) + wi0)
 
 class BayesMLE(Classifier):
     def __init__(self, training_data: Union[Iterable[np.ndarray], np.ndarray], **kwargs):
@@ -21,26 +26,29 @@ class BayesMLE(Classifier):
 
             mu = np.sum(features, axis=0) / np.sum(classes == c)
 
-            memo = features - mu
+            memo = np.matrix(features - mu)
             sigma = memo.T.dot(memo) / np.sum(classes == c)
             mu = mu.reshape(mu.shape[0], 1)
-            print(sigma)
-            exit()
-
-            # Calculate discriminant function
-            psigma = sigma.copy()
-            pmu = mu.copy()
-            W = (-1 / 2) * np.linalg.inv(sigma)
-            w = np.linalg.inv(sigma).dot(mu)
-
-            omega = ((((-1 / 2) * (mu).T).dot(np.linalg.inv(sigma))).dot(mu)) - (-1 / 2) * np.log(np.linalg.det(sigma))
 
             self.parameters[c] = {
-                'mu':pmu.copy(),
-                'sigma':psigma.copy(),
-                'g': make_discriminant(W, w, omega)
+                'mu':mu,
+                'sigma':sigma,
+                'g': make_discriminant(mu.copy(), sigma.copy(), self.priors[c])
             }
-        # end loop
+        p1_data = []
+        p2_data = []
+        num_right = 0
+
+        for tester in training_data:
+            logger.warn(tester[1:])
+            p1 = self.parameters[1]['g'](tester[1:])
+            p2 = self.parameters[2]['g'](tester[1:])
+
+            if tester[0] == (1 if p1 > p2 else 2): num_right += 1
+            logger.info("Pred: %i Actual: %i\tGot p1: %.3f and p2: %.3f" % (1 if p1 > p2 else 2, tester[0], p1, p2))
+            p1_data.append(p1)
+            p2_data.append(p2)
+        logger.info("Got %i right out of %i" % (num_right, len(training_data)))
 
     def __repr__(self):
         return str(self)
